9c062ca378f36afb717cd45ead57bda4f6713b0f
jessie huijsing <22009035@student.hhs.nl>
2024-11-14 12:08:08 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        for (int i = -10; i <= 5; i+=2) {
+
+            System.out.println("optyfen gouw"  + i);
+        }
+    }
+}
